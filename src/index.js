986b767ec32f2e5d08b734ff5fa646e0f9ab5989
Denis40573 <priahind@list.ru>
2025-06-23 11:46:26 +0400

    Added brain-calc app to my project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+
+export const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+
+  const userName = readlineSync.question('May I have your name? ');  
+
+  console.log(`Hello, ${userName}!`);
+  return userName;
+};
+
+export const question = (expression) => {
+  console.log(`Question: ${expression}`);
+
+  const userAnswer = readlineSync.question('Your answer: ');
+  return userAnswer;
+};
+
+export const successfulCompletionGame = (userName) => console.log(`Congratulations, ${userName}!`);
+
+export const correctAnswer = () => console.log('Correct!');
+ 
+export const wrongAnswer = (userAnswer, correctAnswer, userName) => console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}`);
